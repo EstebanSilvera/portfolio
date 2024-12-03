c3368b9ce04d8896d72b82062e295145a40161a0
EstebanSilvera <estebansilvera023@gmail.com>
2024-12-03 12:06:13 -0500

    Changes in repositorie

--- src/component/Repositories.jsx
+++ src/component/Repositories.jsx
@@ -48,7 +48,7 @@ const Repositories = () => {
         <div>
             <div className="cursor z-0" style={{ left: position.x, top: position.y }}></div>
 
-            <div  className='md:flex justify-center items-center p-20 relative z-50' style={{ backgroundImage: `url(${fondo})`, backgroundSize: "cover", backgroundPosition: "center" }} >
+            <div className='md:flex justify-center items-center p-20 relative z-50' style={{ backgroundImage: `url(${fondo})`, backgroundSize: "cover", backgroundPosition: "center" }} >
 
 
                 <div className='bg-white w-72 rounded-2xl relative md:bottom-24'>
@@ -84,9 +84,9 @@ const Repositories = () => {
 
             <div className='container mx-auto flex flex-wrap justify-evenly items-center m-5 relative z-50'>
 
-                <Link to="https://pokedex-mu-sepia.vercel.app/" className='py-4'>
+                <Link to="https://poke-api-delta-blue.vercel.app/" className='py-4'>
                     <div className="max-w-md mx-auto bg-white rounded-xl overflow-hidden md:max-w-2xl">
-                        {/* Contenido de la tarjeta */}
+
                         <img className="w-full h-32 md:h-48 object-cover object-center" src={pokedex} alt="Imagen de la tarjeta" />
                         <div className="p-6">
                             <h2 className="font-semibold text-xl mb-2">pokedex with tablet</h2>
@@ -99,10 +99,8 @@ const Repositories = () => {
                     </div>
                 </Link>
 
-
                 <Link to="https://empremrec.vercel.app/" className='py-4'>
                     <div className="max-w-md mx-auto bg-white rounded-xl overflow-hidden md:max-w-2xl">
-                        {/* Contenido de la tarjeta */}
                         <img className="w-full h-32 md:h-48 object-cover object-center" src="https://www.stelorder.com/wp-content/uploads/2021/09/portada-empresa.jpg" alt="Imagen de la tarjeta" />
                         <div className="p-6">
                             <h2 className="font-semibold text-xl mb-2">My design of enterprise</h2>
@@ -110,14 +108,27 @@ const Repositories = () => {
                         </div>
                         <h2 className="font-semibold text-xl mb-2 px-6 pb-3">🛠 Language and tools</h2>
                         <div className="px-6 pb-3 flex justify-start items-start">
-                            <FaReact className='size-12 mx-6' /><SiAxios className='size-12 mx-6' /><SiTailwindcss className='size-12 mx-6' /><SiVite  className='size-12 mx-6' /><DiMaterializecss   className='size-12 mx-6' />
+                            <FaReact className='size-12 mx-6' /><SiAxios className='size-12 mx-6' /><SiTailwindcss className='size-12 mx-6' /><SiVite className='size-12 mx-6' /><DiMaterializecss className='size-12 mx-6' />
+                        </div>
+                    </div>
+                </Link>
+
+                <Link to="https://ddc-electricts.vercel.app/" className='py-4'>
+                    <div className="max-w-md mx-auto bg-white rounded-xl overflow-hidden md:max-w-2xl">
+                        <img className="w-full h-32 md:h-48 object-cover object-center" src="https://www.enelx.com/content/dam/local-colombia/articulos-personas/banners/2022/07/sistema-fotovoltaico--conoce-c%C3%B3mo-tu-empresa-puede-reducir-el-impacto-ambiental/sistema-solar-fotovoltaico-desktop.jpg" alt="Imagen de la tarjeta" />
+                        <div className="p-6">
+                            <h2 className="font-semibold text-xl mb-2">Enterprise of Energy DDC</h2>
+                            <p className="text-gray-600">At DDc, we are dedicated to revolutionizing the renewable energy landscape by delivering cutting-edge solar energy solutions. Our mission is to harness the power of innovation and advanced technology to provide sustainable, efficient, and affordable energy for a cleaner future. </p>
+                        </div>
+                        <h2 className="font-semibold text-xl mb-2 px-6 pb-3">🛠 Language and tools</h2>
+                        <div className="px-6 pb-3 flex justify-start items-start">
+                            <FaReact className='size-12 mx-6' /><SiAxios className='size-12 mx-6' /><SiTailwindcss className='size-12 mx-6' /><SiVite className='size-12 mx-6' /><DiMaterializecss className='size-12 mx-6' />
                         </div>
                     </div>
                 </Link>
 
                 <Link to="https://inventario-3vre147k7-estebansilveras-projects.vercel.app/" className='py-4'>
                     <div className=" mx-auto bg-white rounded-xl overflow-hidden md:max-w-2xl">
-                        {/* Contenido de la tarjeta */}
                         <img className="w-full h-32 md:h-48 object-cover object-center" src={inventario} alt="Imagen de la tarjeta" />
                         <div className="p-6">
                             <h2 className="font-semibold text-xl mb-2">Inventory of computer and network equipment</h2>
@@ -132,7 +143,6 @@ const Repositories = () => {
 
                 <Link to="https://sufifront.vercel.app/" className='py-4'>
                     <div className="max-w-md mx-auto bg-white rounded-xl overflow-hidden md:max-w-2xl">
-                        {/* Contenido de la tarjeta */}
                         <img className="w-full h-32 md:h-48 object-cover object-center" src={sufi} alt="Imagen de la tarjeta" />
                         <div className="p-6">
                             <h2 className="font-semibold text-xl mb-2">Frontend test</h2>
@@ -140,14 +150,13 @@ const Repositories = () => {
                         </div>
                         <h2 className="font-semibold text-xl mb-2 px-6 pb-3">🛠 Language and tools</h2>
                         <div className="px-6 pb-3 flex justify-start items-start">
-                            <FaReact className='size-12 mx-6' /><FaBootstrap className='size-12 mx-6' /><SiAxios className='size-12 mx-6' /> <SiTailwindcss className='size-12 mx-6' /><SiVite  className='size-12 mx-6' />
+                            <FaReact className='size-12 mx-6' /><FaBootstrap className='size-12 mx-6' /><SiAxios className='size-12 mx-6' /> <SiTailwindcss className='size-12 mx-6' /><SiVite className='size-12 mx-6' />
                         </div>
                     </div>
                 </Link>
 
                 <Link to="https://app-angular-omega.vercel.app/home" className='py-4'>
                     <div className="max-w-md mx-auto bg-white rounded-xl overflow-hidden md:max-w-2xl">
-                        {/* Contenido de la tarjeta */}
                         <img className="w-full h-32 md:h-48 object-cover object-center" src={angular} alt="Imagen de la tarjeta" />
                         <div className="p-6">
                             <h2 className="font-semibold text-xl mb-2">First application with Angular</h2>
@@ -163,7 +172,6 @@ const Repositories = () => {
 
                 <Link to="https://angular-covid.vercel.app/login" className='py-4'>
                     <div className="max-w-md mx-auto bg-white rounded-xl overflow-hidden md:max-w-2xl">
-                        {/* Contenido de la tarjeta */}
                         <img className="w-full h-32 md:h-48 object-cover object-center" src={covid} alt="Imagen de la tarjeta" />
                         <div className="p-6">
                             <h2 className="font-semibold text-xl mb-2">Covid test</h2>
